96b2c82d6693e2b87a97de3d0cea9c3700110048
Karoline Heiwolt <karoline@heiwolt.de>
2022-05-09 16:50:21 +0100

    Changed 3 single-dimension PCAs into one PCA with an coordinate vectors

--- leaf_encoding.py
+++ leaf_encoding.py
@@ -4,81 +4,77 @@ from sklearn.decomposition import PCA
 import numpy as np
 import matplotlib.pyplot as plt
 import visualise
+from sklearn.manifold import TSNE
+import seaborn as sns
 
 def load_inputs(dir):
     leaves = os.listdir(dir)
     data = np.asarray([np.load(os.path.join(dir, leaf)) for leaf in leaves])
-    return data
+    return data, leaves
 
-def fit_pca(sample):
-    x_data = sample[:,:,0]
-    y_data = sample[:,:,1]
-    z_data = sample[:,:,2]
+def get_labels(file_names):
+    '''
+    IDs are Plant, Timestep, and Leaf
+    Note that Leaf Numbers are only consistent within the same plant
+    '''
+    ids = np.asarray([[''.join([letter for letter in word if letter.isnumeric()]) for word in name.split('_')[:3]]for name in file_names], dtype='int')
+    return ids
 
-    pca_x = PCA()
-    pca_y = PCA()
-    pca_z = PCA()
+def fit_pca(data):
+    flat_data = data.reshape(data.shape[0], data.shape[1]*data.shape[2])
+    pca = PCA()
+    pca.fit(flat_data)
+    transformed = pca.fit_transform(flat_data)
 
-    pca_x.fit(x_data)
-    pca_y.fit(y_data)
-    pca_z.fit(z_data)
+    print(f"Using {pca.components_.shape[0]} components")
+    print(f"Explained variance ratio: {sum(pca.explained_variance_ratio_)}")
+    plt.plot(np.cumsum(pca.explained_variance_ratio_[:100]))
+    plt.show()
+    return pca, transformed
 
-    transformed_x = pca_x.fit_transform(x_data)
-    transformed_y = pca_y.fit_transform(y_data)
-    transformed_z = pca_z.fit_transform(z_data)
-
-    print(f"Using {pca_x.components_.shape[0]} components")
-    print(f"Explained variance ration in the x dimension : {sum(pca_x.explained_variance_ratio_)}")
-    print(f"Explained variance ration in the y dimension : {sum(pca_y.explained_variance_ratio_)}")
-    print(f"Explained variance ration in the z dimension : {sum(pca_z.explained_variance_ratio_)}")
-
-    return [pca_x, pca_y, pca_z]
-
-def split_dataset(data, split = 0.2):
+def split_dataset(data, labels, split = 0.2):
     total_nr_examples = data.shape[0]
     nr_test_examples = int(np.floor(split * total_nr_examples))
     indeces = np.random.choice(range(total_nr_examples), size=nr_test_examples, replace=False)
     test_data = data[indeces,:,:]
+    test_labels = labels[indeces,:]
     train_data = np.delete(data, indeces, axis=0)
-    return train_data, test_data
+    train_labels = np.delete(labels, indeces, axis=0)
+    return train_data, test_data, train_labels, test_labels
 
-def perform_single_reprojection(sample, pcas, components=None, draw = False):
+def perform_single_reprojection(sample, pca, components=None, draw = False):
     '''One single leaf (point cloud) at a time'''
     if components is None:
-        components = pcas[0].components_.shape[0]
-    reprojection_parts = []
-    for dimension in range(len(pcas)):
-        query = sample[:,dimension]
-        all_eigenleaves = pcas[dimension].components_
-        query_weight = compress(query, all_eigenleaves[:components], pcas[dimension])
-        reprojection_part = decompress(query_weight, all_eigenleaves[:components], pcas[dimension])
-        reprojection_parts.append(reprojection_part)
-    full_reprojection = np.stack((reprojection_parts), axis = -1)
+        components = pca.components_.shape[0]
 
-    euclidean_dist_error = np.linalg.norm(sample-full_reprojection, axis = 1) # euclidean distance between each point pair
+    original_shape = sample.shape
+    query = sample.reshape(sample.shape[0]*sample.shape[1])
+    all_eigenleaves = pca.components_
+    query_weight = compress(query, all_eigenleaves[:components], pca)
+    reprojection = decompress(query_weight, all_eigenleaves[:components], pca)
+    unrolled = reprojection.reshape(original_shape) # restack
+    euclidean_dist_error = np.linalg.norm(sample-unrolled, axis = 1) # euclidean distance between each point pair
     mean_error = euclidean_dist_error.mean()
     print(f"Mean reprojection error at {components} components : {mean_error}")
 
     if draw:
         cloud1 = visualise.array_to_o3d_pointcloud(sample)
-        cloud2 = visualise.array_to_o3d_pointcloud(full_reprojection)
+        cloud2 = visualise.array_to_o3d_pointcloud(unrolled)
         visualise.draw([cloud1, cloud2], 'test', offset = True )
     return
 
-def test_reprojection_loss(sample, pcas):
-    max_components = pcas[0].components_.shape[0]
+def test_reprojection_loss(sample, pca):
+    original_shape = sample.shape
+    max_components = pca.components_.shape[0]
     mean_errors = []
+    all_eigenleaves = pca.components_
+    query = sample.reshape(sample.shape[0], sample.shape[1]*sample.shape[2])
     for i in range(max_components, 0, -1):
-        reprojection_parts = []
-        for dimension in range(len(pcas)):
-            query = sample[:,:,dimension]
-            all_eigenleaves = pcas[dimension].components_
-            query_weight = compress(query, all_eigenleaves[:i], pcas[dimension])
-            reprojection = decompress(query_weight, all_eigenleaves[:i], pcas[dimension])
-            reprojection_parts.append(reprojection)
-        full_reprojection = np.stack((reprojection_parts), axis = -1)
+        query_weight = compress(query, all_eigenleaves[:i], pca)
+        reprojection = decompress(query_weight, all_eigenleaves[:i], pca)
 
-        euclidean_dist_error = np.linalg.norm(sample-full_reprojection, axis = -1) # euclidean distance between each point pair
+        unrolled = reprojection.reshape(original_shape) # restack
+        euclidean_dist_error = np.linalg.norm(sample-unrolled, axis = -1) # euclidean distance between each point pair
         mean_error = euclidean_dist_error.mean(axis=-1).mean()
         mean_errors.append(mean_error)
 
@@ -86,6 +82,13 @@ def test_reprojection_loss(sample, pcas):
     plt.ylabel('Mean RMSE')
     plt.xlabel('Number of principal components')
     plt.show()
+
+
+def plot_2pcs(data, labels, pca, transformed):
+    import pdb; pdb.set_trace()
+    plt.figure(figsize=(16,10))
+    import pdb; pdb.set_trace()
+    sns.scatterplot(x=transformed[:,0], y=transformed[:,1],hue=labels[:transformed.shape[0],0],palette=sns.color_palette("hls", 7), data=data,legend="full",alpha=0.3)
 
 def compress(query, components, pca):
     query_weight = components @ (query - pca.mean_).T # compress
@@ -97,9 +100,13 @@ def decompress(weights, components, pca):
 
 if __name__== "__main__":
     dir = os.path.join('/home', 'karolineheiwolt','workspace', 'data', 'Pheno4D', '_processed', 'pca_input')
-    data = load_inputs(dir)
-    train, test = split_dataset(data)
-    pcas = fit_pca(train)
-    test_reprojection_loss(test, pcas)
+    data, names = load_inputs(dir)
+    labels = get_labels(names)
+    train_ds, test_ds, train_labels, test_labels = split_dataset(data, labels)
+    pca, transformed = fit_pca(train_ds)
+    #test_reprojection_loss(test_ds, pca)
+    plot_2pcs(data, labels, pca, transformed)
+
+
     for leaf in data:
-        perform_single_reprojection(leaf, pcas, components = 100, draw=True)
+        perform_single_reprojection(leaf, pca, components = 50, draw=True)
